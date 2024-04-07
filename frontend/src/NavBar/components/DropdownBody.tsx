192555d23bcae6ad93ad00c8bed3e007febb560b
KayTee1 <truong.kha@outlook.com>
2024-04-07 19:15:30 +0300

    Refactor NavBar components and add Cart functionality

--- frontend/src/NavBar/components/DropdownBody.tsx
+++ frontend/src/NavBar/components/DropdownBody.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { useCart } from "../../context/CartContext";
 import DropdownItem from "./DropdownItem";
 
@@ -6,36 +7,49 @@ const FavoritesModal = () => {
     <div>
       <p className="text-2xl underline">Favorites</p>
       <div className="flex flex-col p-2 mt-5 max-h-72 overflow-scroll">
-        <DropdownItem title="Item 1" />
-        <DropdownItem title="Item 2" />
-        <DropdownItem title="Item 3" />
-        <DropdownItem title="Item 4" />
-        <DropdownItem title="Item 5" />
-        <DropdownItem title="Item 6" />
+        <p className="text-xl">Log in to see your favorites</p>
       </div>
     </div>
   );
 };
 
 const CartModal = () => {
-  const { cartState } = useCart();
+  const { cartState, handleEmptyCart: empty } = useCart();
+  const navigate = useNavigate();
+  
+  const handleEmptyCart = () => {
+    empty();
+  };
+
+  const handleNavToCheckout = () => {
+    if (cartState.items.length === 0) return;
+    navigate("/checkout");
+  };
   return (
-    <div>
-      <p className="text-2xl underline">Cart</p>
-      <div className="flex flex-col p-2 mt-5 max-h-60 overflow-scroll">
-        {cartState.items.length === 0 ? (
-          <p className="text-xl">Cart is empty</p>
-        ) : (
-          cartState.items.map((item, index: number) => (
-            <DropdownItem key={index} title={item.title} />
-          ))
-        )}
+    <div className="flex flex-col justify-between h-full">
+      <div>
+        <p className="text-2xl underline">Cart</p>
+        <div className="flex flex-col p-2 mt-5 max-h-60 overflow-scroll">
+          {cartState.items.length === 0 ? (
+            <p className="text-xl">Cart is empty</p>
+          ) : (
+            cartState.items.map((item, index: number) => (
+              <DropdownItem key={index} item={item} />
+            ))
+          )}
+        </div>
       </div>
-      <div className="flex flex-row mt-4 justify-center ">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
+      <div className="flex flex-row mt-4 justify-center">
+        <button
+          onClick={handleNavToCheckout}
+          className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
+        >
           Checkout
         </button>
-        <button className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded ml-2">
+        <button
+          onClick={handleEmptyCart}
+          className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded ml-2"
+        >
           Empty Cart
         </button>
       </div>
--- frontend/src/NavBar/components/DropdownItem.tsx
+++ frontend/src/NavBar/components/DropdownItem.tsx
@@ -2,13 +2,22 @@ import { CgHeart } from "react-icons/cg";
 import { MdOutlineRemoveShoppingCart } from "react-icons/md";
 
 type DropdownItemProps = {
-  title: string;
+  item: {
+    id: number;
+    title: string;
+    quantity?: number;
+  };
 };
-const DropdownItem = ({ title }: DropdownItemProps) => {
+const DropdownItem = ({ item }: DropdownItemProps) => {
   return (
     <div className="w-full p-3 my-1 bg-red-100">
       <div className="flex flex-row items-baseline justify-between">
-        <p>{title}</p>
+        <div className="flex flex-row justify-between items-baseline">
+          <p>{item.title}</p>
+          {item.quantity! > 1 ? (
+            <p className="ml-2 text-gray-500 text-xs">{`Qty: ${item.quantity}`}</p>
+          ) : null}
+        </div>
         <div className="flex flex-row w-9 justify-between">
           <button>
             <CgHeart />
