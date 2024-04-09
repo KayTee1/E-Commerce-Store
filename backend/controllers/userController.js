9835c71d493c7f6370fbe26cc272ccef45238193
KayTee1 <truong.kha@outlook.com>
2024-04-09 15:15:20 +0300

    removed getUserById add username property in auth context instead

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -69,6 +69,7 @@ const loginUser = async (req, res) => {
     const token = jwt.sign(
       {
         id: user.id,
+        username: user.username,
         email: user.email,
       },
       process.env.JWT_KEY,
@@ -77,6 +78,7 @@ const loginUser = async (req, res) => {
 
     res.status(200).json({
       id: user.id,
+      username: user.username,
       email: user.email,
       token,
     });
@@ -86,7 +88,4 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = {
-  signUpUser,
-  loginUser,
-};
+module.exports = { signUpUser, loginUser };
