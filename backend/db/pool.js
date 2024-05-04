2d89698fde4c50f2fa05490b72dd8e2afdc57751
KayTee1 <truong.kha@outlook.com>
2024-05-04 20:18:27 +0300

    test hardcoded values

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "db",
+  host: process.env.MYSQL_HOST,
   user: "root",
   password: "password",
   database: "react_store",
